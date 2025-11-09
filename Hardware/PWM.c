ed738d0190223048d368eeb336dc6ec02558267f
unique1201 <642562013@qq.com>
2025-11-09 16:03:18 +0800

    Add files via upload

--- Hardware/PWM.c
+++ Hardware/PWM.c
@@ -10,12 +10,15 @@ void PWM_Init(void)
 	/*开启时钟*/
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);			//开启TIM2的时钟
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);			//开启GPIOA的时钟
+
+	
 	/*GPIO初始化*/
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2|GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);								
+	GPIO_Init(GPIOA, &GPIO_InitStructure);							//将PA2引脚初始化为复用推挽输出	
+																	//受外设控制的引脚，均需要配置为复用模式
 	
 	
 	/*配置时钟源*/
@@ -25,12 +28,12 @@ void PWM_Init(void)
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;				//定义结构体变量
 	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;     //时钟分频，选择不分频，此参数用于配置滤波器时钟，不影响时基单元功能
 	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; //计数器模式，选择向上计数
-	TIM_TimeBaseInitStructure.TIM_Period = 100 - 1;					//计数周期，即ARR的值
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 36 - 1;				//预分频器，即PSC的值
+	TIM_TimeBaseInitStructure.TIM_Period = 100 - 1;                 //计数周期，即ARR的值
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 360 - 1;               //预分频器，即PSC的值
 	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;            //重复计数器，高级定时器才会用到
 	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);             //将结构体变量交给TIM_TimeBaseInit，配置TIM2的时基单元
 	
-	/*输出比较初始化*/
+	/*输出比较初始化*/ 
 	TIM_OCInitTypeDef TIM_OCInitStructure;							//定义结构体变量
 	TIM_OCStructInit(&TIM_OCInitStructure);                         //结构体初始化，若结构体没有完整赋值
 	                                                                //则最好执行此函数，给结构体所有成员都赋一个默认值
@@ -38,9 +41,14 @@ void PWM_Init(void)
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;               //输出比较模式，选择PWM模式1
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;       //输出极性，选择为高，若选择极性为低，则输出高低电平取反
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;   //输出使能
-	TIM_OCInitStructure.TIM_Pulse = 0;		//CCR                   //初始的CCR值
-	TIM_OC3Init(TIM2, &TIM_OCInitStructure);                        //将结构体变量交给TIM_OC1Init，配置TIM2的输出比较通道1
-	TIM_OC4Init(TIM2, &TIM_OCInitStructure);
+	TIM_OCInitStructure.TIM_Pulse = 0;								//初始的CCR值
+	TIM_OC3Init(TIM2, &TIM_OCInitStructure);                        //将结构体变量交给TIM_OC3Init，配置TIM2的输出比较通道3
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;               //输出比较模式，选择PWM模式1
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;       //输出极性，选择为高，若选择极性为低，则输出高低电平取反
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;   //输出使能
+	TIM_OCInitStructure.TIM_Pulse = 0;								//初始的CCR值
+	TIM_OC4Init(TIM2, &TIM_OCInitStructure);                        //将结构体变量交给TIM_OC3Init，配置TIM2的输出比较通道3
+	
 	/*TIM使能*/
 	TIM_Cmd(TIM2, ENABLE);			//使能TIM2，定时器开始运行
 }
@@ -52,11 +60,12 @@ void PWM_Init(void)
   * 注意事项：CCR和ARR共同决定占空比，此函数仅设置CCR的值，并不直接是占空比
   *           占空比Duty = CCR / (ARR + 1)
   */
-void PWM_SetCompare1(uint16_t Compare)
+void PWM_SetCompare3(uint16_t Compare)
 {
-	TIM_SetCompare3(TIM2, Compare);		//设置CCR3(PA2)的值
+	TIM_SetCompare3(TIM2, Compare);		//设置CCR3的值
 }
-void PWM_SetCompare2(uint16_t Compare)
+
+void PWM_SetCompare4(uint16_t Compare)
 {
-	TIM_SetCompare4(TIM2, Compare);		//设置CCR4(PA3)的值
+	TIM_SetCompare4(TIM2, Compare);		//设置CCR3的值
 }
