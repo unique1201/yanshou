46f630a0805682ffaf2ed5ee039bc8fd13a2792a
unique1201 <642562013@qq.com>
2025-11-09 16:09:04 +0800

    Add files via upload

--- Hardware/LED.h
+++ Hardware/LED.h
@@ -2,8 +2,11 @@
 #define __LED_H
 
 void LED_Init(void);
-void LED_ON(void);
-void LED_OFF(void);
-void LED_Turn(void);
+void LED1_ON(void);
+void LED1_OFF(void);
+void LED1_Turn(void);
+void LED2_ON(void);
+void LED2_OFF(void);
+void LED2_Turn(void);
 
 #endif
